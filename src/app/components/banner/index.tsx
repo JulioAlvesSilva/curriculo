174e25ff374ca7cdb80429d37ea7ff34e9576acd
Julio Cesar Alves da <aps.juliocs@gmail.com>
2024-07-12 14:46:10 -0300

    12-07-2024 2

--- src/app/components/banner/index.tsx
+++ src/app/components/banner/index.tsx
@@ -1,6 +1,7 @@
 import styles from './Banner.module.scss';
-import  dados from './dados.json';
-import { FaLinkedin } from "react-icons/fa";
+import dados from './dados.json';
+import { FaLinkedin, FaInstagramSquare, FaFacebookSquare } from "react-icons/fa";
+import { FaSquareGithub } from "react-icons/fa6";
 export default function BannerMain() {
     return (
         <section className={styles.banner}>
@@ -18,9 +19,18 @@ export default function BannerMain() {
                 </div>
             </div>
             <div className={styles.banner_2pg}>
-                    <div className={styles.banner_2pg_soc}>
-                        <FaLinkedin/>
-                    </div>
+                <div className={styles.banner_2pg_soc}>
+                    <FaLinkedin />
+                    <FaSquareGithub />
+                    <FaInstagramSquare />
+                    <FaFacebookSquare />
+                </div>
+                <div className={styles.banner_2pg_resumo}>
+                    <h2>RESUMO</h2>
+                    <p>Sou desenvolvedor Full Stack e dedico-me a criar e aprimorar diversos projetos. Meu foco atual é no desenvolvimento web, utilizando a arquitetura SPA (Single Page Application) como padrão de trabalho. Tenho amplo conhecimento em diversas bibliotecas e frameworks, o que me permite garantir a qualidade e eficiência das minhas aplicações.</p>
+                    <p>Sou fascinado por código limpo e reutilizável. Atualmente, trabalho no ambiente web e utilizo Next.js como minha principal ferramenta. Escolhi Next.js pela sua versatilidade em utilizar client-side e server-side em um único framework. No entanto, também tenho experiência com Angular e Vue.js, e não teria problemas em migrar para outras tecnologias, se necessário.</p>
+                    <p>Para o back-end, utilizo Prisma como facilitador e PostgreSQL/MySQL para banco de dados relacional, além de MongoDB para banco de dados não relacional, e já trabalhei com Spring Boot para a criação de APIs. Sou apaixonado por codificação e estou sempre em busca de aprender e me aprimorar. A facilidade com que absorvo novos conhecimentos é uma das minhas maiores habilidades. Trabalhar como desenvolvedor não é apenas minha profissão, mas minha paixão. Sinto-me realizado e feliz codificando.</p>
+                </div>
             </div>
         </section>
     )
